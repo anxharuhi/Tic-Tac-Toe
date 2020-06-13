c369cf62b84c6a13c1e8558d5d18441eb69f5fe2
llomellamomario <llomellamomario@gmail.com>
2020-06-13 17:46:31 +0200

    Stage 2 completed.  - TitTacToe class initial implementation   - Ability to store a board state   - Ability to print current state to screen with proper formatting

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,19 @@
-# write your code here
-print('X O X\n'
-      'O X O\n'
-      'X X O\n')
+from string import Template
+
+
+class TicTacToe:
+
+    def __init__(self, initial_state):
+        self.state = {'n' + str(key): value for key, value in enumerate(iterable=list(initial_state), start=1)}
+        self.board = Template('---------\n'
+                              '| $n1 $n2 $n3 |\n'
+                              '| $n4 $n5 $n6 |\n'
+                              '| $n7 $n8 $n9 |\n'
+                              '---------\n')
+
+    def print_board(self):
+        print(self.board.safe_substitute(self.state))
+
+
+board = TicTacToe(input())
+board.print_board()
